3e7bb35e69fc7d24545887cb5f59e165a3045fd3
evot <neobeta@outlook.com>
2020-06-11 23:54:37 +0800

    使用自己的注释快捷键

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -460,9 +460,9 @@ If the character before and after CH is space or tab, CH is NOT slash"
   "tfm" 'toggle-frame-maximized
   "ti" 'fastdef-insert
   "th" 'fastdef-insert-from-history
-  "ci" 'evilnc-comment-or-uncomment-lines
+  "cc" 'evilnc-comment-or-uncomment-lines
   "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
-  "cc" 'evilnc-copy-and-comment-lines
+  "cd" 'evilnc-copy-and-comment-lines
   "cp" 'my-evilnc-comment-or-uncomment-paragraphs
   "ct" 'evilnc-comment-or-uncomment-html-tag ; evil-nerd-commenter v3.3.0 required
   "ic" 'my-imenu-comments
