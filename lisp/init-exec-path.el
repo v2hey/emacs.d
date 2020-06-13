6eb5ef648b88209ccfac820f236ddda7ad74e6d2
evot <neobeta@outlook.com>
2020-06-13 13:12:01 +0800

    添加 cache-path-from-shell 可优化项

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,4 +1,6 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
+;; use `cache-path-from-shell` to cache this, save startup time
+;; https://github.com/manateelazycat/cache-path-from-shell
 
 (with-eval-after-load 'exec-path-from-shell
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
